2dea91859f951b0dcbf4e709538cd22921d1a16f
MB-Liz <137509480+MB-Liz@users.noreply.github.com>
2023-10-04 03:11:25 +0400

    add display values and reset values

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
 const calculatorContent = document.querySelector(".calculator-content");
 const buttonsContainer = document.querySelector(".buttons-container");
 const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
+const operatorsButton = document.querySelectorAll(".operators");
 const equal = document.querySelector(".equal");
+const previousOperationDisplay = document.querySelector(".previous-operation-display");
+const currentOperationDisplay = document.querySelector(".current-operation-display");
 
 
 let previousNumber = [];
-let currentNumber = 6;
-let operator = '/';
-operators.forEach(operator => operator.addEventListener("click", () => {
-    console.log(operator.textContent);
-    operator = operator.textContent;
+let currentNumber = [];
+let operator = '';
 
+equal.addEventListener("click", () =>{
+  populateDisplay(previousNumber);
+  console.log(operator);
+  let answer = operate(Math.floor(previousNumber), Math.floor(currentNumber), operator);
+  console.log(answer);
+  populatePreviousDisplay(previousNumber, operator, currentNumber);
+  populateDisplay(answer);
+  previousNumber = [];
+  currentNumber = [];
+  operator = '';
+})
+
+operatorsButton.forEach(operatorButton => operatorButton.addEventListener("click", () => {
+    console.log(operatorButton.textContent);
+    operator = operatorButton.textContent;
+    previousNumber = currentNumber;
+    currentNumber = [];
+    populatePreviousDisplay(previousNumber, operator);
 }));
 
 numbers.forEach(number => number.addEventListener("click", () => {
         console.log(number.textContent);
         currentNumber += number.textContent;
-        console.table(typeof(Math.floor(currentNumber)));
+        console.table(currentNumber);
+        populateDisplay(currentNumber);
 }));
 
 //        console.log(operate(previousNumber, currentNumber, operator));
 
+// Display
+
+function populateDisplay(value){
+  currentOperationDisplay.textContent = value;
+}
+
+function populatePreviousDisplay(value, operator, value2){
+  if (value2 == null){
+    previousOperationDisplay.textContent = `${value} ${operator}`;
+  }else{
+    previousOperationDisplay.textContent = `${value} ${operator} ${value2} =`;
+  }
+}
+
+
+
+
 
 
 function operate(previousNumber, currentNumber, operator){
+    let answer = 0;
     switch(operator){
         case '+':
             answer = addition(previousNumber, currentNumber);
@@ -48,6 +84,7 @@ function operate(previousNumber, currentNumber, operator){
 // addition
 function addition(previousNumber, currentNumber){
   let answer = previousNumber + currentNumber;
+  console.log(answer);
   return answer;
 }
 
@@ -75,9 +112,3 @@ function modulo(previousNumber, currentNumber){
     return answer;
   }
 
-      
-console.log(addition(86, 24));
-console.log(subtraction(86, 24));
-console.log(multiplication(86, 24));
-console.log(division(86, 24));
-console.log(modulo(86, 24));
